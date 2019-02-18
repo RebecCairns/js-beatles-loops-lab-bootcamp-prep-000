6f1394fdcd7b710cc9b3c0bfb7743067d7c36897
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-02-18 19:53:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// add solution here
+
+function theBeatlesPlay(musicianArr, instrumentArr){
+  var newArr = [];
+  for(let i = 0; i < 4; i++){
+    newArr[i] = musicianArr[i] +  " plays " + instrumentArr[i];
+  }
+  return newArr;
+}
+
+function johnLennonFacts(factArr){
+  var factArrLength = factArr.length;
+  var newArr = [];
+  var incrementer = 0;
+  while(incrementer < factArrLength){
+    newArr.push(factArr[incrementer] + "!!!");
+    incrementer++;
+  }
+  return newArr;
+}
+
+function iLoveTheBeatles(num){
+  var times = 15 - num;
+  var newArr = [];
+  var incrementor = 0;
+  while(incrementor < times){
+    newArr.push("I love the Bestles!");
+    incrementor++;
+  }
+}
